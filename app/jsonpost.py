ffe79fec8286b37539fa2004bd633d48fb48558e
qian <rootqf@gmail.com>
2016-01-27 00:16:01 +0800

    blog test

--- app/jsonpost.py
+++ app/jsonpost.py
@@ -1,11 +1,13 @@
 # -*- coding: UTF-8 -*-
 
-from flask import jsonify, request, abort, make_response, current_app
+import json
+from flask import jsonify, request, abort, make_response, current_app, Response
 import simplejson as simplejson
 from functools import wraps
 from app import app
 
 """"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+""" 装饰器 """
 
 
 def allow_cross_domain(fun):
@@ -22,6 +24,19 @@ def allow_cross_domain(fun):
         return rst
 
     return wrapper_fun
+
+
+def support_jsonp(f):
+    """Wraps JSONified output for JSONP"""
+
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        callback = request.args.get('callback', False)
+        if callback:
+            content = str(callback) + '(' + str(f(*args, **kwargs).data) + ')'
+            return current_app.response_class(content, mimetype='application/javascript')
+        else:
+            return f(*args, **kwargs)
 
 
 """"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
@@ -48,32 +63,46 @@ def testjson():
 
 
 @app.route('/jsonpost', methods=['POST'])
-def jsonp(func):
-    """Wraps JSONified output for JSONP requests."""
-
-    @wraps(func)
-    def decorated_function(*args, **kwargs):
-        callback = request.args.get('callback', False)
-        if callback:
-            data = str(func(*args, **kwargs).data)
-            content = str(callback) + '(' + data + ')'
-            mimetype = 'application/javascript'
-            return current_app.response_class(content, mimetype=mimetype)
-        else:
-            return func(*args, **kwargs)
-
-    return decorated_function
+@support_jsonp
+def jsonptest():
+    print request.headers
+    # print request.json
+    # if not request.json:
+    rt = {'id': '12345', 'nickname': 'qian'}
+    # return Response(json.dumps(rt), mimetype='application/json')
+    return jsonify(rt)
 
 
-def jsonpost():
-    if not request.json:
-        abort(400)
-    callback = request.args.get('callback', False)
-    task = {'id': request.json['id'], 'nickname': request.json['nickname']} + ")"
-    print task
-    # return jsonify({'tasks': task})
-    # return HttpResponse(simplejson.dumps({"jsonp": false,"aa": "callbackName"}))
-    return task
+# def jsonp(func):
+#     """Wraps JSONified output for JSONP requests."""
+#
+#     @wraps(func)
+#     def decorated_function(*args, **kwargs):
+#         callback = request.args.get('callback', False)
+#         if callback:
+#             data = str(func(*args, **kwargs).data)
+#             content = str(callback) + '(' + data + ')'
+#             mimetype = 'application/javascript'
+#             return current_app.response_class(content, mimetype=mimetype)
+#         else:
+#             return func(*args, **kwargs)
+#
+#     return decorated_function
+#
+
+
+# def jsonpost():
+#     if not request.json:
+#         abort(400)
+#     print 1
+#     callback = request.args.get('callback', False)
+#     content = str(callback) + '(' + request.json + ')'
+#     print content
+#     task = {'id': request.json['id'], 'nickname': request.json['nickname']} + ")"
+#     print task
+#     # return jsonify({'tasks': task})
+#     # return HttpResponse(simplejson.dumps({"jsonp": false,"aa": "callbackName"}))
+#     return
 
 
 """"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
